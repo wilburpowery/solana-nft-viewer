e7b4fee67f8d948932fad84d580609c033a30d4e
Wilbur Powery <wilburpowery@gmail.com>
2021-09-25 18:35:30 -0600

    remove google fonts preconnect links

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,12 +74,6 @@ export default function Home() {
         /> */}
 
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
 
