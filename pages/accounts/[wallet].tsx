a3561a75af66363783ee72512cf47370e0b95895
Wilbur Powery <wilburpowery@gmail.com>
2021-09-28 19:31:52 -0600

    wip

--- pages/accounts/[wallet].tsx
+++ pages/accounts/[wallet].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import axios from 'axios';
 import dynamic from 'next/dynamic';
@@ -26,19 +26,21 @@ export default function WalletPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
-  if (router.isReady) {
-    axios
-      .get(`/api/get-account-info/${wallet}`)
-      .then((response) => {
-        setError(false);
-        setCollectibles(response.data.nfts);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(true);
-        setLoading(false);
-      });
-  }
+  useEffect(() => {
+    if (router.isReady && collectibles.length == 0) {
+      axios
+        .get(`/api/get-account-info/${wallet}`)
+        .then((response) => {
+          setError(false);
+          setCollectibles(response.data.nfts);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(true);
+          setLoading(false);
+        });
+    }
+  });
 
   return (
     <div className="min-h-screen p-8 text-white bg-black">
@@ -58,7 +60,26 @@ export default function WalletPage() {
 
         {loading ? (
           <div className="flex items-center justify-center w-full mt-32 animate-spin">
-            <span className="text-3xl">👀</span>
+            <svg
+              className="w-10 h-10 mx-auto mr-3 -ml-1 text-white animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
           </div>
         ) : (
           ''
@@ -81,6 +102,7 @@ export default function WalletPage() {
           plugins={[lgThumbnail]}
           elementClassNames="mt-8 grid grid-cols-2 gap-2 sm:grid-cols-3 lg:grid-cols-4"
           selector=".item"
+          licenseKey="AAC7666D-D0114A77-8C96F80F-E927E68C"
           alignThumbnails="right"
         >
           {collectibles?.map((nft) => (
