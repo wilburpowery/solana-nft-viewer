72b99bc6fd89dd111fba64454149200508177627
Wilbur Powery <wilburpowery@gmail.com>
2021-10-07 15:56:27 -0600

    Add content loader svg for loading state

--- pages/accounts/[wallet].tsx
+++ pages/accounts/[wallet].tsx
@@ -7,6 +7,8 @@ const LightGallery = dynamic(() => import('lightgallery/react'), {
   ssr: false,
 });
 
+import ContentLoader from 'react-content-loader';
+
 // import styles
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-thumbnail.css';
@@ -55,32 +57,27 @@ export default function WalletPage({ wallet, nfts }) {
           </Link>
         </div>
 
-        {/* {loading ? (
-          <div className="flex items-center justify-center w-full mt-32 animate-spin">
-            <svg
-              className="w-10 h-10 mx-auto mr-3 -ml-1 text-white animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+        {!data ? (
+          <div className="grid grid-cols-2 gap-2 mt-8 sm:grid-cols-3 lg:grid-cols-4">
+            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => (
+              <ContentLoader
+                className="rounded-md"
+                speed={2}
+                width="100%"
+                height="auto"
+                viewBox="0 0 500 490"
+                backgroundColor="#10142a"
+                foregroundColor="#22283E"
+              >
+                <rect x="207" y="112" rx="0" ry="0" width="1" height="1" />
+                <rect x="262" y="172" rx="0" ry="0" width="0" height="1" />
+                <rect x="0" y="0" rx="0" ry="0" width="500" height="490" />
+              </ContentLoader>
+            ))}
           </div>
         ) : (
           ''
-        )} */}
+        )}
 
         {error ? (
           <div className="flex flex-col items-center justify-center w-full mt-32">
