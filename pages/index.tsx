072c5e19e4cc6aa1d1865499e0f16e72429353a8
Wilbur Powery <wilburpowery@gmail.com>
2021-10-10 21:45:15 -0600

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,9 @@ export default function Home() {
           property="og:description"
           content="Scan any Solana wallet address and see all the NFTs in a beautiful gallery."
         />
-        <meta property="og:image" content="/og.png/" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:image" content="/og.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://solnft.gallery" />
@@ -69,7 +71,7 @@ export default function Home() {
           property="twitter:description"
           content="Scan any Solana wallet address and see all the NFTs in a beautiful gallery."
         />
-        <meta property="twitter:image" content="/og.png/" />
+        <meta property="twitter:image" content="/og.png" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
