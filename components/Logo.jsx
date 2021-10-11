03237b7e94a2273e7e2f39c3ac6f5400f8996fa9
Wilbur Powery <wilburpowery@gmail.com>
2021-10-10 21:14:26 -0600

    Add logo & open graph image

---
+++ components/Logo.jsx
@@ -0,0 +1,198 @@
+export default function Logo({classes}) {
+    return (
+        <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`fill-current mx-auto mb-2 h-36 ${classes}`}
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="50 80 280 200"
+              version="1.0"
+            >
+              <defs>
+                <path
+                  d="M7.93.262c3.636 0 6.726-2.145 6.726-5.785 0-7.215-8.969-5.426-8.969-8.805 0-1.106.844-1.625 1.883-1.625 1.106 0 2.211.617 3.184 1.687l2.86-2.73c-1.43-1.656-3.606-2.762-6.009-2.762-3.09 0-6.242 1.856-6.242 5.754 0 6.629 9.004 5.035 9.004 8.45 0 1.07-.91 1.784-2.406 1.784-1.79 0-3.184-1.007-3.899-2.535L.52-4.094C1.79-1.656 4.516.262 7.93.262Zm0 0"
+                  id="a"
+                />
+                <path
+                  d="M8.254.262c4.355 0 7.215-2.86 7.215-6.762 0-3.898-2.86-6.758-7.215-6.758-4.356 0-7.246 2.86-7.246 6.758 0 3.902 2.89 6.762 7.246 6.762Zm0-3.672c-1.79-.035-3.086-1.332-3.086-3.09 0-1.754 1.297-3.055 3.086-3.086 1.754.031 3.086 1.332 3.086 3.086 0 1.758-1.332 3.055-3.086 3.09Zm0 0"
+                  id="b"
+                />
+                <path d="M1.918 0h4.16v-22.258h-4.16Zm0 0" id="c" />
+                <path
+                  d="M15.922-19.496v12.898L6.336-19.496H1.918V0h4.289v-12.934L15.757 0h4.423v-19.496Zm0 0"
+                  id="d"
+                />
+                <path
+                  d="M13.809-15.727v-3.77H2.079V0h4.288v-7.863h6.371v-3.77h-6.37v-4.094Zm0 0"
+                  id="e"
+                />
+                <path
+                  d="M13.941-19.496H-.164v3.8h4.941V0h4.258v-15.695h4.906Zm0 0"
+                  id="f"
+                />
+                <path
+                  d="M11.21-10.691v3.347h5.36c-.91 2.047-2.922 3.41-5.328 3.41-3.281 0-5.816-2.53-5.816-5.816 0-3.281 2.535-5.816 5.816-5.816 2.309 0 4.192 1.238 5.168 3.09l3.703-1.919c-1.687-3.183-5.035-5.363-8.87-5.363A9.993 9.993 0 0 0 1.233-9.75 9.993 9.993 0 0 0 11.242.262C16.766.262 21.25-4.223 21.25-9.75v-.941Zm0 0"
+                  id="g"
+                />
+                <path
+                  d="M16.41-3.379c-.555 0-1.137-.195-1.137-1.27v-8.347h-4.128v1.137c-.973-.88-2.207-1.399-3.704-1.399-3.605 0-6.433 2.988-6.433 6.758S3.836.262 7.44.262c1.883 0 3.41-.848 4.454-2.211C12.703-.617 14.137.26 15.89.26c.421 0 .812-.132 1.105-.261v-3.379ZM8.254-3.41A3.066 3.066 0 0 1 5.168-6.5a3.063 3.063 0 0 1 3.086-3.086 3.088 3.088 0 0 1 0 6.176Zm0 0"
+                  id="h"
+                />
+                <path
+                  d="M15.14-6.531c0-4.063-2.792-6.758-6.984-6.758-4.191 0-7.148 2.73-7.148 6.789 0 4.063 2.926 6.762 7.277 6.762 3.61 0 6.012-1.918 6.66-4.746H10.95c-.355 1.039-1.297 1.656-2.726 1.656-1.887 0-2.86-1.137-3.024-2.988h9.942Zm-6.917-3.574c1.394 0 2.207.648 2.629 1.785H5.457c.426-1.102 1.3-1.785 2.766-1.785Zm0 0"
+                  id="i"
+                />
+                <path
+                  d="M9.715-13.258c-1.527 0-2.793.781-3.637 2.047v-1.785h-4.16V0h4.16v-6.207c0-2.145 1.461-3.379 3.508-3.379.488 0 .91.066 1.203.129v-3.672c-.293-.098-.684-.129-1.074-.129Zm0 0"
+                  id="j"
+                />
+                <path
+                  d="M10.594-12.996 7.41-4.386l-3.25-8.61H-.164L5.266.293 2.21 7.797h4.351l8.32-20.793Zm0 0"
+                  id="k"
+                />
+                <clipPath id="l">
+                  <path d="M119.223 96.125h136.5v132.64h-136.5Zm0 0" />
+                </clipPath>
+              </defs>
+              <use
+                x="96.888"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#a"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="111.768"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#b"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="127.753"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#c"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="135.226"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#d"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="156.832"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#e"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="170.445"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#f"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="183.701"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#g"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="205.664"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#h"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="222.624"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#c"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="230.097"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#c"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="237.569"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#i"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="253.164"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#j"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <use
+                x="263.366"
+                y="260.775"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xlinkHref="#k"
+                xlinkType="simple"
+                xlinkActuate="onLoad"
+                xlinkShow="embed"
+                fill="fillCurrent"
+              />
+              <g clipPath="url(#l)">
+                <path
+                  fill="fillCurrent"
+                  d="M255.688 137.797v67.281c0 13.082-10.602 23.688-23.684 23.688l-24.258-41.336c8.793-4.895 14.742-14.278 14.742-25.051 0-15.828-12.828-28.656-28.652-28.656a28.526 28.526 0 0 0-15.086 4.293l-14.023-23.903H232c13.086 0 23.688 10.602 23.688 23.684Zm-61.848 53.238c-15.828 0-28.656-12.832-28.656-28.656a28.53 28.53 0 0 1 6.23-17.832l-28.48-48.531c-13.079 0-23.68 10.605-23.68 23.687v67.277c0 13.083 10.601 23.692 23.68 23.692h67.273l-11.742-20.016c-1.508.242-3.05.38-4.625.38Zm0 0"
+                />
+              </g>
+              <path
+                fill="fillCurrent"
+                d="M193.668 140.902c-11.836 0-21.434 9.594-21.434 21.434 0 11.84 9.594 21.437 21.434 21.437 11.836 0 21.437-9.597 21.437-21.437 0-11.836-9.597-21.434-21.437-21.434Zm0 28.293a6.858 6.858 0 1 1 6.86-6.86 6.853 6.853 0 0 1-6.86 6.86Zm0 0"
+              />
+        </svg>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,10 @@ import { validatePublicKey } from '../utils/utils';
 
 import { useRouter } from 'next/router';
 
+import Image from 'next/image';
+
+import Logo from '../components/Logo';
+
 export default function Home() {
   useEffect(() => {
     document.getElementById('wallet').focus();
@@ -53,10 +57,7 @@ export default function Home() {
           property="og:description"
           content="Scan any Solana wallet address and see all the NFTs in a beautiful gallery."
         />
-        {/* <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        /> */}
+        <meta property="og:image" content="/logo.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://solnft.gallery" />
@@ -68,10 +69,7 @@ export default function Home() {
           property="twitter:description"
           content="Scan any Solana wallet address and see all the NFTs in a beautiful gallery."
         />
-        {/* <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        /> */}
+        <meta property="twitter:image" content="/logo.png" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
@@ -79,7 +77,7 @@ export default function Home() {
 
       <div className="absolute right-10 top-8">
         <a
-          href="https://twitter.com/wilburpowery"
+          href="https://twitter.com/solnftgallery"
           className="text-gray-400 hover:text-white"
         >
           <svg
@@ -99,51 +97,8 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center min-h-screen mx-auto max-w-7xl">
         <div className="flex flex-col justify-center p-8 rounded-lg bg-dark-tertiary">
           <div>
-            <svg
-              width="400"
-              height="400"
-              viewBox="0 0 400 400"
-              className="w-16 h-auto mx-auto mb-2"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="200" cy="200" r="200" fill="black" />
-              <g clipPath="url(#clip0)">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M123.42 255.13C124.173 254.302 125.09 253.641 126.113 253.188C127.135 252.735 128.242 252.501 129.36 252.5L312.64 252.65C313.421 252.652 314.184 252.88 314.837 253.307C315.491 253.734 316.006 254.342 316.32 255.056C316.635 255.77 316.735 256.561 316.609 257.331C316.483 258.101 316.136 258.818 315.61 259.395L276.58 302.37C275.827 303.198 274.909 303.86 273.886 304.313C272.862 304.766 271.755 305 270.635 305L87.3602 304.85C86.5797 304.848 85.8164 304.62 85.1631 304.193C84.5098 303.766 83.9946 303.158 83.6801 302.444C83.3656 301.73 83.2652 300.939 83.3913 300.169C83.5173 299.399 83.8644 298.682 84.3902 298.105L123.42 255.13ZM315.61 219.355C316.136 219.932 316.483 220.649 316.609 221.419C316.735 222.189 316.635 222.98 316.32 223.694C316.006 224.408 315.491 225.016 314.837 225.443C314.184 225.87 313.421 226.098 312.64 226.1L129.365 226.25C128.246 226.25 127.139 226.016 126.115 225.563C125.091 225.11 124.173 224.448 123.42 223.62L84.3902 180.62C83.8644 180.043 83.5173 179.326 83.3913 178.556C83.2652 177.786 83.3656 176.995 83.6801 176.281C83.9946 175.567 84.5098 174.959 85.1631 174.532C85.8164 174.105 86.5797 173.877 87.3602 173.875L270.64 173.725C271.759 173.726 272.865 173.96 273.888 174.413C274.911 174.866 275.828 175.527 276.58 176.355L315.61 219.355ZM123.42 97.63C124.173 96.8023 125.09 96.1408 126.113 95.6879C127.135 95.2351 128.242 95.0007 129.36 95L312.64 95.15C313.421 95.1516 314.184 95.3798 314.837 95.8069C315.491 96.234 316.006 96.8416 316.32 97.5559C316.635 98.2703 316.735 99.0606 316.609 99.8308C316.483 100.601 316.136 101.318 315.61 101.895L276.58 144.87C275.827 145.698 274.909 146.36 273.886 146.813C272.862 147.266 271.755 147.5 270.635 147.5L87.3602 147.35C86.5797 147.348 85.8164 147.12 85.1631 146.693C84.5098 146.266 83.9946 145.658 83.6801 144.944C83.3656 144.23 83.2652 143.439 83.3913 142.669C83.5173 141.899 83.8644 141.182 84.3902 140.605L123.42 97.63Z"
-                  fill="url(#paint0_linear)"
-                />
-              </g>
-              <defs>
-                <linearGradient
-                  id="paint0_linear"
-                  x1="90.4202"
-                  y1="309.58"
-                  x2="309.58"
-                  y2="90.42"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#9945FF" />
-                  <stop offset="0.2" stopColor="#7962E7" />
-                  <stop offset="1" stopColor="#00D18C" />
-                </linearGradient>
-                <clipPath id="clip0">
-                  <rect
-                    width="240"
-                    height="210"
-                    fill="white"
-                    transform="translate(80 95)"
-                  />
-                </clipPath>
-              </defs>
-            </svg>
+            <Logo />
           </div>
-          <h1 className="mb-4 text-4xl font-bold text-center md:text-5xl font-heading">
-            NFT Gallery
-          </h1>
-
           <p className="mb-4">
             Enter a wallet address and view your entire collection.
           </p>
